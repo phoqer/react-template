97bdabaab71c084c5d3c4931a1a01ff3e37fc379
imykhailychenko <igor.c.m@ukr.net>
2022-12-03 22:09:28 +0100

    update

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 
-import { useOpen, Container } from 'phoqer';
+import { useOpen, Container, Link } from 'phoqer';
 import { Appear } from 'phoqer-shared';
 
 const HomePage = (): JSX.Element => {
@@ -21,6 +21,9 @@ const HomePage = (): JSX.Element => {
         <Appear>
             <Container>
                 <h1>React template</h1>
+                <Link href="https://phoqer.github.io/react-template/" target="_blank">
+                    https://phoqer.github.io/react-template/
+                </Link>
             </Container>
         </Appear>
     );
